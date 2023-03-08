abe62843fade5f0d9dfe98bda88ea348d19f5cd8
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-03-07 17:26:56 -0700

    add Matthew's content

---
+++ ruby_exercises/mythical-creatures/lib/vampire.rb
@@ -0,0 +1,18 @@
+class Vampire
+    attr_reader :name, :pet, :thirsty
+    def initialize(name, pet = "bat", thirsty = true)
+        @name = name
+        @pet = pet
+        @thirsty = true
+    end
+
+    def bat?
+        return true if pet == "bat"
+        false
+    end
+
+    def drink
+        @thirsty = false
+    end
+    
+end
