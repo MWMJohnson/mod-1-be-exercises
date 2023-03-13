db961cbc6eab7fe2c9850cef7587c0c1d6576258
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-03-12 21:32:11 -0600

    Solve the first three hash test.

--- ruby_exercises/data-types/collections/spec/hashes_spec.rb
+++ ruby_exercises/data-types/collections/spec/hashes_spec.rb
@@ -1,21 +1,24 @@
 RSpec.describe 'Hashes' do
   it 'test 1' do
     # In the line below, create a new empty hash called empty
-    empty = ______
+    empty = {}
     expect(empty).to eq({})
   end
 
-  xit 'test 2' do
+  it 'test 2' do
     # In the line below, create an empty hash with a default value 0
-    empty = ______
+    empty = { not_found: 0
+    }
     expect(empty[:not_found]).to eq(0)
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     # In the line below, create a hash called ages. The hash should
     # have a key of "ben" with a value of 4 and a key of "kelly" with
     # a value of 6
-    ages = ______
+
+    ages = {"ben" => 4, "kelly" => 6}
+
     expect(ages.length).to eq(2)
     expect(ages["ben"]).to eq(4)
     expect(ages["kelly"]).to eq(6)
