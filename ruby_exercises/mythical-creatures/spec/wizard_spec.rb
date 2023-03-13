10cd08bd2d4337454953af47345398a01cc84f34
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-03-12 20:36:13 -0600

    Add code to test  and  methods.

--- ruby_exercises/mythical-creatures/spec/wizard_spec.rb
+++ ruby_exercises/mythical-creatures/spec/wizard_spec.rb
@@ -34,19 +34,36 @@ RSpec.describe Wizard do
 
   it 'starts rested' do
     # create wizard
+    wizard = Wizard.new('Harry')
     # .rested? returns true
+  expect(wizard.rested?).to be true
   end
 
   it 'can cast spells' do
     # create wizard
+    wizard = Wizard.new('Perry')
     # .cast returns "MAGIC MISSILE!"
+    expect(wizard.cast).to eq "MAGIC MISSLE!"
   end
 
   it 'gets tired after casting three spells' do
+
     # create wizard
+    wizard = Wizard.new('Sherry', bearded: false)
+
     # casts spell twice
+    2.times do
+      wizard.cast
+    end
+
     # check if wizard is rested
+    expect(wizard.rested?).to be true
+
     # casts spell
+    wizard.cast
+
     # check wizard is not rested
+    expect(wizard.rested?).to be false
+
   end
 end
